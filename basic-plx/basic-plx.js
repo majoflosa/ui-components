7bb1e43dca77499a89ce8c49f5021b349ee281c9
Mauricio Flores <contact@maurojflores.com>
2019-02-02 17:17:59 -0600

    Basic parallax component now checks if element has been scrolled into view

--- basic-plx/basic-plx.js
+++ basic-plx/basic-plx.js
@@ -7,22 +7,26 @@ window.addEventListener('load', () => {
          * @param { string } backgroundElementSelector css selector for element wrapping background img
          * @param { string } backgroundImageSelector css selector for background img
          */
-        constructor( selectors ) {
+        constructor( options ) {
             // the element that wraps content and parallaxed background
-            this.$banner = document.querySelector( selectors.bannerSelector );
+            this.$banner = document.querySelector( options.bannerSelector );
             // the element containing the image to use as background for the banner
-            this.$bannerBg = this.$banner.querySelector( selectors.backgroundSelector );
+            this.$bannerBg = this.$banner.querySelector( options.backgroundSelector );
 
             // the height of the wrapper element
             this.bannerHt = this.$banner.offsetHeight;
             // the height of the image used as background
-            this.bannerBgHt = this.$bannerBg.querySelector( selectors.backgroundImageSelector ).offsetHeight;
+            this.bannerBgHt = this.$bannerBg.querySelector( options.backgroundImageSelector ).offsetHeight;
 
             // value to use for `top` css property on image used as background;
             // if the banner is full-screen height, set to 0; else, center vertically
             this.initialBannerTop = this.$banner.classList.contains('full-screen') 
                 ? 0 
                 : -(this.bannerBgHt - this.bannerHt) / 2;
+
+            // amount of pixels the background image should move on scroll,
+            // proportional to the amount of pixels window scrolls
+            this.scrollRatio = options.scrollRatio || 0.1;
             
             // bind context of all class methods to the created instance object
             this.init = this.init.bind( this );
@@ -50,7 +54,17 @@ window.addEventListener('load', () => {
          * Calculate and set new `top` value for banner image based on window scroll position
          */
         parallax() {
-            this.$bannerBg.style.top = this.initialBannerTop - (window.scrollY * 0.15) + 'px';
+            // whether or not the parallax banner is currently scrolled into view
+            this.elementIsVisible = (
+                this.$banner.offsetTop - window.innerHeight <= window.scrollY
+                    && window.scrollY <= this.$banner.offsetTop + (this.bannerHt)
+            );
+            
+            // if banner is not currently in view, do nothing
+            if ( !this.elementIsVisible ) return false;
+
+            // re-adjust the image's position
+            this.$bannerBg.style.top = this.initialBannerTop - (window.scrollY * this.scrollRatio) + 'px';
         }
     }
 
@@ -58,7 +72,8 @@ window.addEventListener('load', () => {
     const plxBanner = new BasicPlxBanner( {
         bannerSelector: '.hero-banner',
         backgroundSelector: '.plx-background', 
-        backgroundImageSelector: '.plx-background-img'} 
-    );
+        backgroundImageSelector: '.plx-background-img',
+        scrollRatio: -0.2 // defaults to 0.1
+    });
 
 });
