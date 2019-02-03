53b188df19882ceac030f397ad7a35881025085f
Mauricio Flores <contact@maurojflores.com>
2019-02-03 16:19:34 -0600

    Refactored and documented layered-plx.js; added transpiled min version

--- layered-plx/README.md
+++ layered-plx/README.md
@@ -3,3 +3,86 @@
 A banner section with 2 or more layers of elements with parallax effect. Each layer can be given its own scroll-speed ratio.
 
 [See live demo](http://ui.maurojflores.com/ui-components/layered-plx/layered-plx.html)
+
+## Usage Example
+**HTML**
+```html
+<div class="layered-plx">
+    <!-- optional parallax background image -->
+    <div class="plx-background">
+        <img class="plx-background-img" src="/path/to/image">
+    </div>
+
+    <div class="plx-layer plx-layer-1"><h1>Layer 1</h1></div>
+    <div class="plx-layer plx-layer-2"><h1>Layer 2</h1></div>
+    <div class="plx-layer plx-layer-3"><h1>Layer 3</h1></div>
+</div>
+```
+
+**CSS**
+```css
+.layered-plx {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  min-height: 500px;
+}
+
+.plx-background {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.plx-layer {
+  position: absolute;
+}
+
+/* Adjust these styles for your own layers' design */
+.plx-layer-1 {
+  right: 30px;
+  background: #dddddd;
+  padding: 10px 0;
+  width: 20%;
+  text-align: center;
+}
+
+.plx-layer-2 {
+  right: 200px;
+  background: grey;
+  padding: 30px 0;
+  width: 33%;
+  text-align: center;
+  color: #ffffff;
+}
+
+.plx-layer-3 {
+  right: 40%;
+  background: #333333;
+  padding: 50px 0;
+  width: 50%;
+  text-align: center;
+  color: #ffffff;
+}
+```
+
+**JavaScript**
+```javascript
+const layeredPlxBanner = new LayeredPlxBanner({
+    bannerSelector: '.layered-plx', // query selector for wrapping element of parallax banner
+    plxBackgroundSelector: '.plx-background', // query selector for element containing background image; optional
+    plxBackgroundImgSelector: '.plx-background-img', // query selector for background image; optional
+    plxBackgroundScrollRatio: 0.15, // proportional shift of background image to scroll amount; defaults to 0.15
+    layerSelector: '.plx-layer', // query selector for other parallax layers
+    layerOptions: [ 
+        // options for each layer; adjust according to desired layer initial position and parallax speed
+        { initialTop: 600, scrollRatio: 0.5 }, // layer 1; default values
+        { initialTop: 800, scrollRatio: 0.95 }, // layer 2
+        { initialTop: 1100, scrollRatio: 1.5 } // layer 3
+    ]
+});
+```
--- layered-plx/layered-plx.js
+++ layered-plx/layered-plx.js
@@ -1,92 +1,99 @@
-window.addEventListener('load', () => {
-
-    class LayeredPlxBanner {
-        /**
-         * 
-         * @param {object} elements object containing css selectors for parallax banner wrapper, background image, and
-         * array of layers
-         */
-        constructor( elements ) {
-            // the collection of selectors and elements that will have parallax effect
-            this.elements = elements;
-
-            // the element that wraps content and parallaxed background
-            this.$banner = document.querySelector( elements.banner );
-            // the element containing the image to use as background for the banner
-            this.$bannerBg = this.$banner.querySelector( elements.plxBackground );
-
-            // the height of the wrapper element
-            this.bannerHt = this.$banner.offsetHeight;
-            // the height of the image used as background
-            this.bannerBgHt = this.$bannerBg.querySelector( elements.plxBackgroundImg ).offsetHeight;
-
-            // value to use for `top` css property on image used as background;
-            // if the banner is full-screen height, set to 0; else, center vertically
-            this.initialBannerTop = this.$banner.classList.contains('full-screen') 
-                ? 0 
-                : -(this.bannerBgHt - this.bannerHt) / 2;
-
-            // bind context of all class methods to the created instance object
-            this.init = this.init.bind( this );
-            this.bindEvents = this.bindEvents.bind( this );
-            this.parallax = this.parallax.bind( this );
-
-            // run all initial class functionality
-            this.init();
+class LayeredPlxBanner {
+    /**
+     * 
+     * @param {object} options object containing css selectors for parallax banner wrapper, background image, and
+     * array of layers
+     */
+    constructor( options ) {
+        // the element that wraps content and parallax background
+        this.$banner = document.querySelector( options.bannerSelector );
+        if ( !this.$banner ) {
+            console.warn(`The query selector ${options.bannerSelector} did not match any elements on the document. Aborting parallax function.`);
+            return false;
         }
 
-        /**
-         * Set initial `top` position of image being used as background,
-         * Bind event listeners to pertinent DOM elements
-         */
-        init() {
+        this.layerElements = [...document.querySelectorAll( options.layerSelector )];
+        this.layers = this.layerElements.map( (layerElement, index) => {
+            return {
+                element: layerElement,
+                initialTop: options.layerOptions[index]
+                    ? options.layerOptions[index].initialTop
+                    : 600,
+                scrollRatio: options.layerOptions[index]
+                    ? options.layerOptions[index].scrollRatio 
+                    : 0.5
+            };
+        });
+        if ( !this.layers.length ) {
+            console.warn(`The query selector ${options.layerSelector} did not match any elements on the document. Aborting parallax function.`);
+            return false;
+        }
+
+        // the element containing the optional image to use as background for the banner
+        this.$bannerBg = this.$banner.querySelector( options.plxBackgroundSelector );
+        // the optional image to use as background for the banner
+        this.$bannerBgImg = this.$bannerBg 
+            ? this.$bannerBg.querySelector( options.plxBackgroundImgSelector )
+            : null;
+        // the height of the wrapper element
+        this.bannerHt = this.$banner.offsetHeight;
+        // parallax background is optional
+        this.bannerBgHt = this.$bannerBg && this.$bannerBgImg
+            ? this.$bannerBgImg.offsetHeight
+            : 0;
+        this.plxBackgroundScrollRatio = options.plxBackgroundScrollRatio || 0.15;
+
+        // value to use for `top` css property on image used as background;
+        // if the banner is full-screen height, set to 0; else, center vertically
+        this.initialBannerTop = this.$banner.classList.contains('full-screen') 
+            ? 0 
+            : -(this.bannerBgHt - this.bannerHt) / 2;
+
+        // bind context of all class methods to the created instance object
+        this.bindEvents = this.bindEvents.bind( this );
+        this.parallax = this.parallax.bind( this );
+
+        // initialize background position, if any
+        if ( this.$bannerBg )
             this.$bannerBg.style.top = this.initialBannerTop + 'px';
-            this.bindEvents();
-        }
-
-        bindEvents() {
-            window.addEventListener( 'scroll', this.parallax );
-        }
-
-        /**
-         * Calculate and set new `top` value for banner image and layers based on window scroll position
-         */
-        parallax() {
-            // calculate amount of px to shift background element's position and set to `top` property
-            this.$bannerBg.style.top = this.initialBannerTop - (window.scrollY * 0.15) + 'px';
-            // calculate amount of px to shift each layer element and set to `top` property
-            this.elements.layers.forEach( layer => {
-                layer.element.style.top = layer.initialTop - (window.scrollY * layer.scrollRatio ) + 'px';
-            });
-        }
+        
+        // bind all events to respective handlers handlers
+        this.bindEvents();
     }
 
-    // select all layer elements
-    const layerElements = [...document.querySelectorAll( '.plx-layer' )];
+    bindEvents() {
+        window.addEventListener( 'scroll', this.parallax );
+    }
 
-    // modify layer elements collection to include initial properties
-    const layers = layerElements.map( (layerElement, index) => {
-        let layerObj = {
-            element: layerElement,
-            initialTop: 600,
-            scrollRatio: 0.5
-        };
+    /**
+     * Calculate and set new `top` value for banner image and layers based on window scroll position
+     */
+    parallax() {
+        // calculate amount of px to shift background element's position and set to `top` property
+        if ( this.$bannerBg )
+            this.$bannerBg.style.top = this.initialBannerTop - (window.scrollY * this.plxBackgroundScrollRatio) + 'px';
+        
+        // calculate amount of px to shift each layer element and set to `top` property
+        this.layers.forEach( layer => {
+            layer.element.style.top = layer.initialTop - (window.scrollY * layer.scrollRatio ) + 'px';
+        });
+    }
+}
 
-        return layerObj;
-    });
-
-    // manually override individual initial position and scroll speed ratio
-    layers[1].initialTop = 800;
-    layers[1].scrollRatio = 0.95;
-    layers[2].initialTop = 1100;
-    layers[2].scrollRatio = 1.5;
-
+window.addEventListener('load', () => {
     // initiate the component
     const layeredPlxBanner = new LayeredPlxBanner({
-        banner: '.hero-banner', // css selector for wrapping element of parallax banner
-        plxBackground: '.plx-background', // css selector for element containing background image
-        plxBackgroundImg: '.plx-background-img', // css selector for background imag
-        layers: layers // array of objects containing layer DOM elements and their initial settings
+        bannerSelector: '.layered-plx', // query selector for wrapping element of parallax banner
+        plxBackgroundSelector: '.plx-background', // query selector for element containing background image; optional
+        plxBackgroundImgSelector: '.plx-background-img', // query selector for background image; optional
+        plxBackgroundScrollRatio: 0.15, // proportional shift of background image to scroll amount; defaults to 0.15
+        layerSelector: '.plx-layer', // query selector for other parallax layers
+        layerOptions: [
+            // options for each layer; adjust according to desired layer initial position and parallax speed
+            { initialTop: 600, scrollRatio: 0.5 },
+            { initialTop: 800, scrollRatio: 0.95 },
+            { initialTop: 1100, scrollRatio: 1.5 }
+        ]
     });
 
 });
