76b96ac56c34171cd850564c60b03a41dd136e09
Mauricio Flores <contact@maurojflores.com>
2019-02-04 07:57:27 -0600

    Minor refactoring, documentation and min file for multiple-plx

--- multiple-plx/README.md
+++ multiple-plx/README.md
@@ -1,5 +1,56 @@
 # Multiple Parallax Sections
 
-An example of a page with more than one section with parallax effect. This example combines sections with parallax effect and sections with `background-attachment: fixed`.
+An example of a page with more than one section with parallax effect. 
 
+This example combines sections with parallax effect and sections with `background-attachment: fixed`.
 [See live demo](http://ui.maurojflores.com/ui-components/multiple-plx/multiple-plx.html)
+
+## Basic Usage
+**HTML**
+```html
+<div id="banner-1">
+    <div class="plx-background">
+        <img src="/path/to/image" class="plx-background-img">
+    </div>
+</div>
+<div id="banner-2">
+    <div class="plx-background">
+        <img src="/path/to/image" class="plx-background-img">
+    </div>
+</div>
+```
+
+**CSS**
+```css
+#banner-1, #banner-2 {
+    position: relative;
+    overflow: hidden;
+}
+.plx-background {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+.plx-background-img {
+    width: 100%;
+}
+```
+
+**JavaScript**
+```javascript
+const plxBanner = new MultiplePlxBanner({
+    bannerSelector: '#banner-1', 
+    backgroundSelector: '.plx-background', 
+    backgroundImageSelector: '.plx-background-img',
+    scrollRatio: -0.15 // 0.15 is the default value
+});
+
+const plxBanner2 = new MultiplePlxBanner({
+    bannerSelector: '#banner-2', 
+    backgroundSelector: '.plx-background', 
+    backgroundImageSelector: '.plx-background-img', 
+    scrollRatio: -0.15
+});
+```
--- multiple-plx/multiple-plx.js
+++ multiple-plx/multiple-plx.js
@@ -1,84 +1,89 @@
-window.addEventListener('load', () => {
-    
-    class MultiplePlxBanner {
-        /**
-         * 
-         * @param { string } bannerSelector css selector for wrapping parallax element
-         * @param { string } backgroundElementSelector css selector for element wrapping background image
-         * @param { string } backgroundImageSelector css selector for background image
-         * @param { number } scrollRatio proportion of how many pixels to shift background to pixels scrolled
-         */
-        constructor( selectors ) {
-            // the element that wraps content and parallaxed background
-            this.$banner = document.querySelector( selectors.bannerSelector );
-            // the element containing the image to use as background for the banner
-            this.$bannerBg = this.$banner.querySelector( selectors.backgroundSelector );
-
-            // the height of the wrapper element
-            this.bannerHt = this.$banner.offsetHeight;
-            // the height of the image used as background
-            this.bannerBgHt = this.$bannerBg.querySelector( selectors.backgroundImageSelector ).offsetHeight;
-
-            // value to use for `top` css property on image used as background;
-            // if the banner is full-screen height, set to 0; else, center vertically
-            this.initialBannerTop = this.$banner.classList.contains('full-screen') 
-                ? 0 
-                : -(this.bannerBgHt - this.bannerHt) / 2;
-
-            this.scrollRatio = selectors.scrollRatio;
-            
-            // bind context of all class methods to the created instance object
-            this.init = this.init.bind( this );
-            this.bindEvents = this.bindEvents.bind( this );
-            this.parallax = this.parallax.bind( this );
-
-            // run all initial class functionality
-            this.init();
+class MultiplePlxBanner {
+    /**
+     * 
+     * @param { string } bannerSelector css selector for wrapping parallax element
+     * @param { string } backgroundElementSelector css selector for element wrapping background image
+     * @param { string } backgroundImageSelector css selector for background image
+     * @param { number } scrollRatio proportion of how many pixels to shift background to pixels scrolled
+     */
+    constructor( options ) {
+        // the element that wraps content and parallax background
+        this.$banner = document.querySelector( options.bannerSelector );
+        // abort everything if there is no banner
+        if ( !this.$banner ) {
+            console.warn(`The query selector "${options.bannerSelector}" did not match any elements on the document. Aborting parallax function.`);
+            return false;
+        }
+        // the element containing the image to use as background for the banner
+        this.$bannerBg = this.$banner.querySelector( options.backgroundSelector );
+        // abort everything if there is no banner background element
+        if ( !this.$bannerBg ) {
+            console.warn(`The query selector "${options.backgroundSelector}" did not match any elements on the document. Aborting parallax function.`);
+            return false;
         }
 
-        /**
-         * Set initial `top` position of image being used as background,
-         * Calculate total vertical space above the banner element
-         * Bind event listeners to pertinent DOM elements
-         */
-        init() {
-            this.$bannerBg.style.top = this.initialBannerTop + 'px';
-            this.bindEvents();
+        this.$bannerBgImg = this.$bannerBg.querySelector( options.backgroundImageSelector );
+        // abort everything if there is no banner background image element
+        if ( !this.$bannerBgImg ) {
+            console.warn(`The query selector "${options.backgroundImageSelector}" did not match any elements on the document. Aborting parallax function.`);
+            return false;
         }
 
-        bindEvents() {
-            window.addEventListener( 'scroll', this.parallax );
-        }
+        // the height of the wrapper element
+        this.bannerHt = this.$banner.offsetHeight;
+        // the height of the image used as background
+        this.bannerBgHt = this.$bannerBgImg.offsetHeight;
+
+        // value to use for `top` css property on image used as background
+        this.initialBannerTop = -(this.bannerBgHt - this.bannerHt) / 2;
+
+        this.scrollRatio = options.scrollRatio || 0.15;
         
-        /**
-         * Calculate and set new `top` value for banner image based on window scroll position
-         */
-        parallax() {
-            let isElementVisible = this.$banner.offsetTop - window.innerHeight <= window.scrollY
-                && window.scrollY <= this.$banner.offsetHeight + (this.bannerHt*2);
-                
-            if ( isElementVisible ) {
-                // amount of pixels to shift background, based on element's vertical position on the page
-                const plxScroll = window.scrollY * this.scrollRatio;
-                // set new `top` value on background
-                this.$bannerBg.style.top = this.initialBannerTop - plxScroll + 'px';
-            }
-        }
+        // bind context of all class methods to the created instance object
+        this.bindEvents = this.bindEvents.bind( this );
+        this.parallax = this.parallax.bind( this );
+
+        // set initial `top` value
+        this.$bannerBg.style.top = this.initialBannerTop + 'px';
+        // bind all necessary events to respective handlers
+        this.bindEvents();
     }
 
+    bindEvents() {
+        window.addEventListener( 'scroll', this.parallax );
+    }
+    
+    /**
+     * Calculate and set new `top` value for banner image based on window scroll position
+     */
+    parallax() {
+        // whether or not banner element has been scrolled into view
+        const isElementVisible = this.$banner.offsetTop - window.innerHeight <= window.scrollY
+            && window.scrollY <= this.$banner.offsetHeight + (this.bannerHt*2);
+            
+        if ( isElementVisible ) {
+            // amount of pixels to shift background, based on element's vertical position on the page
+            const plxScroll = window.scrollY * this.scrollRatio;
+            // set new `top` value on background
+            this.$bannerBg.style.top = this.initialBannerTop - plxScroll + 'px';
+        }
+    }
+}
+
+window.addEventListener('load', () => {
     // initiate the component
     const plxBanner = new MultiplePlxBanner({
         bannerSelector: '#banner-1', 
         backgroundSelector: '.plx-background', 
         backgroundImageSelector: '.plx-background-img',
-        scrollRatio: 0.15 // 0.15 is the default value
+        scrollRatio: -0.15 // 0.15 is the default value
     });
 
     const plxBanner2 = new MultiplePlxBanner({
         bannerSelector: '#banner-3', 
         backgroundSelector: '.plx-background', 
         backgroundImageSelector: '.plx-background-img', 
-        scrollRatio: 0.25
+        scrollRatio: -0.15
     });
 
 });
